38deef49bd7c6ff380fafa543c59646197e8317b
Valen-7x <valen.deorigen@gmail.com>
2023-09-26 14:34:15 -0300

    ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    // Agrega cualquier otro archivo donde se utilicen clases de Tailwind
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      // Agrega las mismas rutas de archivos aquí
-    ],
-  },
-};
+}
